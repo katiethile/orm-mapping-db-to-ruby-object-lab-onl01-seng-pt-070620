2c5b1fee590da72fe5b6327603ed49535c5277b2
Katie Le <lethikatie@gmail.com>
2020-09-09 04:26:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,7 @@ end
       LIMIT ?
     SQL
     
-      DB[:conn].execute(sql, name).map do |row|
+      DB[:conn].execute(sql, number).map do |row|
         self.new_from_db(row)
       end
   end 
