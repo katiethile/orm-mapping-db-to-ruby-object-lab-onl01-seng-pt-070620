6bf3dfee11b69330ff871250e196edc9a9568e09
Katie Le <lethikatie@gmail.com>
2020-09-09 04:05:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,7 +94,7 @@ end
   
   def self.first_X_students_in_grade_10(grade)
    sql = <<-SQL
-      SELECT *
+      SELECT FIRST students 
       FROM students 
       WHERE grade = 10
       ORDER BY id 
