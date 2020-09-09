493bf160d771cf889d4b8a347eed1107e1894287
Katie Le <lethikatie@gmail.com>
2020-09-09 03:46:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,6 +93,12 @@ end
   end
   
   def self.first_X_students_in_grade_10
-    
+   sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql)
   end 
 end 
